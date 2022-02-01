ff161e1f11fc7ad47a15e5bb5adc850720c3d2ea
vpaithankar <vishal.paithankar@gmail.com>
2022-02-01 13:15:28 +0530

    Update index.js

--- HttpTriggerApp/index.js
+++ HttpTriggerApp/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
     if(req.method == 'GET') {
     const productId = (req.query.productId || (req.body && req.body.productId));
     const responseMessage = productId
-        ? "The product name for your product id {"+productId+"} is Starfruit Explosion"
+        ? "V2 - The product name for your product id {"+productId+"} is Starfruit Explosion"
         : "Please pass productId in query string or body";
 
     context.res = {
