1aee1fdb1c6c690a38f2bd911ba0bf7e70a42659
vpaithankar <vishal.paithankar@gmail.com>
2022-02-01 12:06:46 +0530

    Add files via upload

---
+++ HttpTriggerApp/index.js
@@ -0,0 +1,25 @@
+module.exports = async function (context, req) {
+    context.log(req);
+    if(req.method == 'GET') {
+    const productId = (req.query.productId || (req.body && req.body.productId));
+    const responseMessage = productId
+        ? "The product name for your product id {"+productId+"} is Starfruit Explosion"
+        : "Please pass productId in query string or body";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+    } else if(req.method == 'POST') {
+        const productId = (req.query.productId || (req.body && req.body.productId));
+        const responseMessage = productId
+        ? "The product name for your product id {"+productId+"} is Starfruit Explosion"
+        : "Please pass productId in query string or body";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+    }
+
+}
